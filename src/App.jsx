c9e1206c078a895c33302b75fb5edbd4c40e046f
Mila Oktay <milavaspopova@gmail.com>
2022-11-13 16:10:56 +0100

    Zoom on search implemented

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,39 @@ import { MyMap } from "./MyMap";
 const url = "http://api.citybik.es/v2/networks";
 
 function App() {
-  const [foundProviders, setFoundProviders] = useState([]);
   const [allProviders, setAllProviders] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
+  const [foundProviders, setFoundProviders] = useState([]);
 
+  //Location
+  const [position, setPosition] = useState([]);
+  const [status, setStatus] = useState(null);
+  const [zoom, setZoom] = useState(10);
+
+  //Get location from user
+  useEffect(() => {
+    (() => {
+      if (!navigator.geolocation) {
+        setStatus("Geolocation is not supported by your browser");
+      } else {
+        setStatus("Locating...");
+        navigator.geolocation.getCurrentPosition(
+          (position) => {
+            setStatus(null);
+            setPosition([position.coords.latitude, position.coords.longitude]);
+          },
+          () => {
+            //If the user declines geolocation, display zoomed out map of the whole world
+            setStatus(null);
+            setPosition([50, 10]);
+            setZoom(2);
+          }
+        );
+      }
+    })();
+  }, []);
+
+  //Fetch data of all bike providers
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -21,13 +50,23 @@ function App() {
     };
     fetchData();
   }, []);
+
+  //Filter data on search
   function handleInput(event) {
     setSearchTerm(event.target.value);
+    //Update providers list
     setFoundProviders(
       allProviders.filter((ele) =>
         ele.location.city.toLowerCase().includes(searchTerm)
       )
     );
+    //Update map position to searched city
+    setPosition([
+      foundProviders[0].location.latitude,
+      foundProviders[0].location.longitude,
+    ]);
+    //Zoom in on searched city
+    setZoom(10);
   }
 
   return (
@@ -35,6 +74,9 @@ function App() {
       <h1>Bike Rental Providers</h1>
       <MyMap
         providers={searchTerm.length < 1 ? allProviders : foundProviders}
+        status={status}
+        position={position}
+        zoom={zoom}
       />
 
       <input
@@ -43,11 +85,14 @@ function App() {
         placeholder="Search city"
       ></input>
 
-      {(searchTerm.length < 1 ? allProviders : foundProviders).map((ele) => (
-        <li key={ele.id}>
-          {ele.name} {ele.location.city} ({ele.location.country})
-        </li>
-      ))}
+      {
+        //Display providers list
+        (searchTerm.length < 1 ? allProviders : foundProviders).map((ele) => (
+          <li key={ele.id}>
+            {ele.name} {ele.location.city} ({ele.location.country})
+          </li>
+        ))
+      }
     </div>
   );
 }
--- src/MyMap.jsx
+++ src/MyMap.jsx
@@ -1,36 +1,12 @@
 import React from "react";
 import { Map, Marker, ZoomControl } from "pigeon-maps";
-import { useEffect, useState } from "react";
 
 export function MyMap(props) {
-  const providers = props.providers;
-  const [lat, setLat] = useState(null);
-  const [lng, setLng] = useState(null);
-  const [status, setStatus] = useState(null);
-
-  useEffect(() => {
-    (async () => {
-      if (!navigator.geolocation) {
-        setStatus("Geolocation is not supported by your browser");
-      } else {
-        setStatus("Locating...");
-        await navigator.geolocation.getCurrentPosition(
-          (position) => {
-            setStatus(null);
-            setLat(position.coords.latitude);
-            setLng(position.coords.longitude);
-          },
-          () => {
-            setStatus("Unable to retrieve your location");
-          }
-        );
-      }
-    })();
-  }, []);
-  console.log(status, lat, lng);
+  const { providers, status, zoom } = props;
+  const [lat, lng] = props.position;
 
   return status == null ? (
-    <Map height={500} defaultCenter={[lat, lng]} defaultZoom={9}>
+    <Map height={500} center={[lat, lng]} zoom={zoom}>
       {providers.map((ele, i) => (
         <Marker
           key={i}
